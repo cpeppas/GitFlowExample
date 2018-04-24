d7b632280c34176634223e3ff43a6a5f1dc0a1c3
Christos Peppas <cpeppas@gmail.com>
2018-04-24 11:44:40 +0100

    adding new in MainCativity

--- app/src/main/java/com/example/mygitflowexample/MainActivity.kt
+++ app/src/main/java/com/example/mygitflowexample/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //todo cnp - implement
         //todo gdpr - implement
+        //todo NEW - implement
     }
 }
