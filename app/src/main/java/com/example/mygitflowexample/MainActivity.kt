14acdba106df6e0155b9f6c7169b0c6c28fd7e81
Christos Peppas <peppas.chris@gmail.com>
2018-04-24 11:47:13 +0100

    Feature/gdpr (#11)
    * add styles
    
    * adding new in MainCativity
    
    * adding NEW2 in MainActivty

--- app/src/main/java/com/example/mygitflowexample/MainActivity.kt
+++ app/src/main/java/com/example/mygitflowexample/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //todo cnp - implement
         //todo gdpr - implement
+        //todo NEW - implement
+        //todo NEW2 - implement
     }
 }
